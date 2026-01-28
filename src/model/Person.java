1c3eecfb132918d0567de6d11f5c9a0e141261fb
MauroPB30 <patinobem93@gmail.com>
2026-01-28 09:52:04 +0100

    Creacion de clase abstract person

---
+++ src/model/Person.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author usuario
+ */
+public abstract class Person {
+    
+        private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
