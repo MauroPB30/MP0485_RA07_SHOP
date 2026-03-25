7023f1c0e80e4c2e9cb50df40de12d7e56d3d760
MauroPB30 <patinobem93@gmail.com>
2026-03-25 12:28:20 +0100

    Update Fichero

--- src/model/Fichero.java
+++ src/model/Fichero.java
@@ -1,67 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import model.Product;
 
-/**
- *
- * @author usuario
- */
 public class Fichero {
 
-    public static void EscrituraFichero(ArrayList<Person> Personas) {
+    public static void LeerInventario(ArrayList<Product> inventory) {
 
-        FileWriter outputPerson;
-        BufferedWriter out1 = null;
+        String linea;
 
-    }
+        String nombre = null;
 
-    public static void FileReader(ArrayList<Product> inventory) {
+        Amount wholesalerprice = new Amount(0.0);
 
-        String line;
+        int stock = 0;
 
-        String name;
-        Amount wholesalesPrice;
-        int stock;
-
-        FileReader inputS = null;
+        FileReader inputStream = null;
         BufferedReader in = null;
 
         try {
-            inputS = new FileReader("inputInventory.txt");
-            in = new BufferedReader(inputS);
+            inputStream = new FileReader("inputInventory.txt");
+            in = new BufferedReader(inputStream);
 
-            while ((line = in.readLine()) != null) {
+            while ((linea = in.readLine()) != null) {
 
-                String data[] = line.split(";");
+                String datos[] = linea.split(";");
 
-                name = data[0];
-                wholesalesPrice = new Amount(Double.parseDouble(data[1]));
-                stock = Integer.parseInt(data[2]);
+                for (int i = 0; i < datos.length; i++) {
+                    String finalDatos[] = datos[i].split(":");
+                    if (finalDatos[0].equals("Product")) {
+                        nombre = finalDatos[1];
+                    } else if (finalDatos[0].equals("Wholesaler Price")) {
+                        wholesalerprice = new Amount(Double.parseDouble(finalDatos[1]));
+                    } else if (finalDatos[0].equals("Stock")) {
+                        stock = Integer.parseInt(finalDatos[1]);
 
-                for (int i = 0; i < data.length - 1; i++) {
-                    String finalDatos[] = line.split(":");
-//                finalDatos = (data);
+                    }
 
-                    name = finalDatos[1];
-                    wholesalesPrice = new Amount(Double.parseDouble(finalDatos[3]));
-                    stock = Integer.parseInt(data[5]);
-
-                    Product p = new Product(name, wholesalesPrice, stock);
-                    inventory.add(p);
                 }
-
+                Product p = new Product(nombre, wholesalerprice, stock);
+                inventory.add(p);
             }
 
         } catch (java.io.IOException ex) {
+            System.out.println(ex);
             System.out.println("No se puede acceder al archivo.");
         } finally {
             if (in != null) {
@@ -69,6 +56,53 @@ public class Fichero {
                     in.close();
                 } catch (IOException ex) {
                     System.out.println("No se puede acceder al archivo.");
+                }
+            }
+        }
+
+    }
+
+    public static void EscribirInventario(ArrayList<Sale> sales) {
+
+//         FileWriter outputStreamProducto;
+        BufferedWriter out1 = null;
+
+        try {
+
+            String date = java.time.LocalDate.now().toString();
+            String fileName = "sales_" + date + ".txt";
+//             outputStreamProducto = new FileWriter("sales_yyyy-mm-dd.txt");
+//             out1 = new BufferedWriter(outputStreamProducto);
+            out1 = new BufferedWriter(new FileWriter(fileName));
+            int index = 1;
+
+            for (Sale sale : sales) {
+
+                out1.write(index + " Cliente: " + sale.getClient().getName() + "\n");
+
+                out1.write(index + " Product: ");
+                for (Product p : sale.getProducts()) {
+                    out1.write((p.getName() + ", " + p.getPublicPrice() + "; "));
+                }
+
+                out1.newLine();
+                out1.write(index + " Amount: " + sale.getAmount() + "\n");
+//                String products = index + ";Products=";
+
+                index++;
+//                }
+
+                System.out.println("Ventas exportadas correctamente");
+            }
+        } catch (java.io.IOException ex) {
+            System.out.println(ex);
+            System.out.println("Error al escribir ventas.");
+        } finally {
+            if (out1 != null) {
+                try {
+                    out1.close();
+                } catch (IOException ex) {
+                    System.out.println("Error al cerrar fichero.");
                 }
             }
         }
